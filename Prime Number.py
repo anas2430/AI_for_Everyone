990afd54c11da7c4ffd4cd2ab82ba9d2f5b94f80
Syed Anas Ali <syedanasali@outlook.com>
2019-06-16 00:40:12 +0500

    Check prime number with user input and also print table with this

---
+++ Prime Number.py
@@ -0,0 +1,12 @@
+
+num = int(input("Enter a number: "))  
+for a in range(1,11):
+     print(num,'x',a,'=',num*a)
+if num > 1:  
+   for i in range(2,num):  
+       if (num % i) == 0:  
+           print(num,"is not a prime number")
+   else:  
+       print(num,"is a prime number") 
+      
+         
